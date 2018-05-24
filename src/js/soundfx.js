4a350137839ceacbe88b4b40525af9d0b9aea403
Ignacio Segura <nacho@isegura.es>
2018-05-24 12:38:59 +0200

    Fadeout engine

--- src/js/soundfx.js
+++ src/js/soundfx.js
@@ -3,12 +3,28 @@
 class SoundFX {
   constructor(sound = null) {
     this.sound = new Audio(sound);
+    this.sound.volume = 1;
+    this.sound.fadeoutTime = 3000;
+    this.fadeInterval;
+
     this.sound.load();
   }
 
   play() {
     this.sound.play();
   }
+
+  fadeOut() {
+    let soundFrame = 10;
+    let rate = 1 * soundFrame / this.fadeoutTime;
+
+    this.fadeInterval = setInterval(
+      () => {
+        this.sound.volume = this.sound.volume - rate;
+        if (this.sound.volume <= 0) clearInterval(this.fadeInterval);
+      }, soundFrame);
+  }
+
 }
 
 export default SoundFX;
