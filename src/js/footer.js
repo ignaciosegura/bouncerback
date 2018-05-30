65a74aa93fbd952666468ac317e4a997b1a6c2ee
Ignacio Segura <nacho@isegura.es>
2018-05-30 11:04:26 +0200

    More readable

--- src/js/footer.js
+++ src/js/footer.js
@@ -7,8 +7,10 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <footer><a rel="nofollow" href="http://www.niknak.es" target="_blank">© Nik Nak Studio</a> /&nbsp;
-      <a rel="nofollow" href="https://github.com/ignaciosegura/bouncerback" target="_blank">Bouncerback on GitHub</a></footer>
+    return <footer>
+      <a rel="nofollow" href="http://www.niknak.es" target="_blank">© Nik Nak Studio</a> /&nbsp;
+      <a rel="nofollow" href="https://github.com/ignaciosegura/bouncerback" target="_blank">Bouncerback on GitHub</a>
+    </footer>
   }
 }
 
