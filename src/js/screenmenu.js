6aa5c83ed43442249685a52e444bad95af8fb677
Ignacio Segura <nacho@isegura.es>
2018-06-05 23:51:04 +0200

    Better reset for bounces

--- src/js/screenmenu.js
+++ src/js/screenmenu.js
@@ -9,12 +9,12 @@ import { Link } from 'react-router-dom';
 import GameShop from './stores/gameshop.js';
 
 class ScreenMenu extends React.Component {
-  resetScores() {
+  componentWillUnmount() {
     GameShop.resetBounces();
   }
   render() {
     return <div className="screen-menu">
-      <Link to="/game" className="text ready" onClick={this.resetScores}>New Game</Link>
+      <Link to="/game" className="text ready">New Game</Link>
       <Link to="/tutorial" className="text ready">Tutorial</Link>
     </div>
   }
