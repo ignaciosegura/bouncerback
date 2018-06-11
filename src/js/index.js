ec56ca79634e4b67eb854181313f261c7d965969
Ignacio Segura <nacho@isegura.es>
2018-06-11 20:56:26 +0200

    Removing unused import

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ require('../.htaccess');
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router, Route, Switch, browserHistory } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory'
 export const history = createBrowserHistory();
 
