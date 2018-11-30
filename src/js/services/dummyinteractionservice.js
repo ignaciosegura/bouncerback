1cdabb1cc14e93f6d197d382dd4f8a14beb46d2d
Ignacio Segura <nacho@isegura.es>
2018-11-30 10:51:10 +0100

    Updated song and service

--- src/js/services/dummyinteractionservice.js
+++ src/js/services/dummyinteractionservice.js
@@ -2,14 +2,15 @@
 // Chrome requires the user to interact with the website before letting any audio to be played.
 // In order to start the Main Screen soundtrack, we need this.
 
+/* global require */
+
 export default class DummyInteractionService {
   static makeDummyInteraction() {
     let body = document.getElementsByTagName('body')[0];
     let dummyID = 'dummy-interaction';
-    let dummyElement = '<button type="button" id="' + dummyID + '"  />';
+    let silencePath = require('../../sound/silence.mp3');
+    let dummyElement = `<iframe id="${dummyID}" src="${silencePath}" allow="autoplay" id="audio" style="display:none"></iframe>`;
 
     body.insertAdjacentHTML('beforeend', dummyElement);
-
-    document.getElementById(dummyID).click();
   }
 }
